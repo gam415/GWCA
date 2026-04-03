7321f34650b7ccea6330c094f3449d2b2ea97844
Mike Jerred <mjerred.work@gmail.com>
2026-04-03 15:55:42 +0100

    fix: update

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   let mut data = fs::read(path)?;
 
-  const SCAN_CODE_LENGTH: usize = 8;
-  let scan_code: [u8; SCAN_CODE_LENGTH] = [0xf7, 0x40, 0x10, 0x00, 0x00, 0x01, 0x00, 0x74];
-  const TOTAL_COUNT: usize = 3;
-  let mut count = 0;
+  const SCAN_CODE_LENGTH: usize = 5;
+  let scan_code: [u8; SCAN_CODE_LENGTH] = [0x2d, 0x20, 0x00, 0x00, 0x10];
+  const INNER_SCAN_CODE_LENGTH: usize = 5;
+  let inner_scan_code: [u8; INNER_SCAN_CODE_LENGTH] = [0x59, 0x59, 0x84, 0xc0, 0x75];
+
   for i in 0..(data.len() - SCAN_CODE_LENGTH) {
     let bytes = &data[i..i+SCAN_CODE_LENGTH];
     if bytes == scan_code {
-      data[i+7] = 0xEB; // change JZ to JMP
-      count = count + 1;
-      if count >= TOTAL_COUNT {
-        break;
+      let mut count_remaining = 2;
+
+      for j in 0..0x200 {
+        let bytes = &data[i+j..i+j+INNER_SCAN_CODE_LENGTH];
+        if bytes == inner_scan_code {
+          data[i+j+4] = 0xEB; // change JNZ to JMP
+          count_remaining = count_remaining - 1;
+
+          if count_remaining == 0 {
+            fs::OpenOptions::new()
+              .write(true)
+              .truncate(true)
+              .open(path)?
+              .write_all(&data)?;
+
+            return Ok(());
+          }
+        }
       }
     }
   }
 
-  if count < TOTAL_COUNT {
-    return Err(format!("Only found {count}/{TOTAL_COUNT} instances of minipet target checks").into());
-  }
-
-  fs::OpenOptions::new()
-    .write(true)
-    .truncate(true)
-    .open(path)?
-    .write_all(&data)?;
-
-  return Ok(());
+  return Err(format!("Could not find minipet target checks").into());
 }
